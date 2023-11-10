bbbe5df30d19d016ae046a4dbe9b70419bd1c9bb
Mordadin <112402508+Mordadin@users.noreply.github.com>
2023-11-10 02:06:13 +0100

    Bruker registrering.

---
+++ Ijustkeeptryingiguess/Migrations/20231109233454_InitialDatabaseSetup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ijustkeeptryingiguess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabaseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalesLead",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn), // MariaDB identity
+                    FirstName = table.Column<string>(type: "varchar(255)", nullable: false), // Changed to varchar(255)
+                    LastName = table.Column<string>(type: "varchar(255)", nullable: false), // Changed to varchar(255)
+                    Mobile = table.Column<string>(type: "varchar(255)", nullable: false), // Changed to varchar(255)
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false), // Changed to varchar(255)
+                    Source = table.Column<string>(type: "varchar(255)", nullable: false) // Changed to varchar(255)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesLead", x => x.Id);
+                });
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesLead");
+        }
+    }
+}
---
+++ Ijustkeeptryingiguess/Migrations/20231110002502_FixedSalesLeadForMariaDB.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ijustkeeptryingiguess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSalesLeadForMariaDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "SalesLead",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "SalesLead",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "SalesLead",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "SalesLead",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "SalesLead",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "SalesLead",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "SalesLead",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "SalesLead",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "SalesLead",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "SalesLead",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
