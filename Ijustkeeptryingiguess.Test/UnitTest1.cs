c4dee970ae405ef24e803616a22140123da99b9c
Shekina22 <lokotoshekinah@gmail.com>
2023-11-25 14:26:32 +0100

    Ijustkeeptrying.Test

---
+++ Ijustkeeptryingiguess.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ijustkeeptryingiguess.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
